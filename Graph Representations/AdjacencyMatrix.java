0f342437087fc31b0b10de2036b183f81cb7e584
shaiksaif7287 <dsaifshaik@gmail.com>
2025-06-18 23:49:34 +0530

    Add files via upload

---
+++ Graph Representations/AdjacencyMatrix.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+public class AdjacencyMatrix{
+    public static void addEdge(int[][] mat,int i,int j){
+        mat[i][j]=1;
+        mat[j][i]=1;
+    }
+    public static void displayMatrix(int[][] mat){
+        for(int[] row:mat){
+            for(int val:row){
+                System.out.print(val+" ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args){
+        int V=4;
+        int[][] mat=new int[V][V];
+        addEdge(mat,0,1);
+        addEdge(mat,0,2);
+        addEdge(mat,1,2);
+        addEdge(mat,2,3);
+        addEdge(mat,1,3); 
+        System.out.println("Adjacency Matrix:");
+        displayMatrix(mat);
+    }
+}
